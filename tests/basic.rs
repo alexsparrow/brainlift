081b682f4aa07eccdb6122890e72904d24d47435
Alex Sparrow <alex@quartic.io>
2020-05-12 00:07:06 +0100

    Check outputs are equal

--- tests/basic.rs
+++ tests/basic.rs
@@ -4,12 +4,16 @@ use brainlift::{stdlib::{MockStdLib, DefaultStdLib}, state::BrainfuckState};
 
 fn compare_interp_jit(code: &str) {
     let mut state1 = BrainfuckState::new();
+    let mut stdlib1 = MockStdLib::new("");
     brainfuck_jit_state(code, &mut DefaultStdLib {}, &mut state1);
 
     let mut state2 = BrainfuckState::new();
+    let mut stdlib2 = MockStdLib::new("");
     brainfuck_state(code, &mut DefaultStdLib {}, &mut state2);
 
     state1.assert_eq(&state2);
+
+    assert_eq!(stdlib1.output, stdlib2.output);
 }
 
 #[test]
